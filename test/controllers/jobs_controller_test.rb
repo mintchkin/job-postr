4c7679862d43cda65ca65bbb50b24e0695976c05
mintchkin <mintchkin.dobfner@gmail.com>
2017-08-11 13:37:05 -0400

    Added basic test for job routes

--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -4,4 +4,14 @@ class JobsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get jobs index" do
+    get jobs_url
+    assert_response :success
+  end
+
+  test "should get new job" do
+    get new_job_url
+    assert_response :success
+  end
 end
