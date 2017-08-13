9cf6d4a388f75247bdc016dc92e968d6a4ecb27b
mintchkin <mintchkin.dobfner@gmail.com>
2017-08-13 16:37:34 -0400

    Added basic job create/index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,16 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
   def new
+    @job = Job.new
+  end
+  def create
+    @job = Job.new(params.require(:job).permit(:title, :description))
+    if @job.save
+      redirect_to jobs_path, flash: {success: "Job Post Created"}
+    else
+      render :new
+    end
   end
 end
